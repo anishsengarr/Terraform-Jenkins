313086dd6165150eaf29c979e041b134bbdd3b39
Anish Singh <99667363+anishsengarr@users.noreply.github.com>
2024-10-02 18:14:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  ami           = "ami-05fa00d4c63e32376" 
   instance_type = "t2.micro"
+
   tags = {
-      Name = "TF-Instance"
+      Name = "Anish-instance"
   }
 }
